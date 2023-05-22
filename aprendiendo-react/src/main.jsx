4db3aaa8bffe10f0f9f53535653e55ab48cd7f32
Leandro Chazarreta <lchaz85@gmail.com>
2023-05-21 22:03:44 -0300

    Creación de Json

--- aprendiendo-react/src/main.jsx
+++ aprendiendo-react/src/main.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Algo from './algo'
 import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render( //solo permite insertar un elemento
-  <React.Fragment> // permite insertar mas de un elemento
-    <div>'Hola'</div>
+//permite insertar mas de un elemento
+  <React.Fragment>  
+    <div><Algo /><Algo /></div>
+    <App />
+    {/* aas */}
+    
     <button>Hola</button>
   </React.Fragment>
 )
